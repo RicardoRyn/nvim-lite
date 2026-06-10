8436fc350992f26808ecfa1cde8965159eb98cfe
Ricardo_T8 <ricardoryn1317@gmail.com>
2026-06-10 17:03:28 +0800

    refactor(pack): improve plugin loading and auto-cleanup

--- lua/config/pack.lua
+++ lua/config/pack.lua
@@ -1,7 +1,7 @@
-local specs = {
+local active_specs = {
   --core
   { src = "https://github.com/catppuccin/nvim", name = "catppuccin" },
-  { src = "https://github.com/mason-org/mason.nvim.git" },
+  { src = "https://github.com/mason-org/mason.nvim" },
   { src = "https://github.com/nvim-treesitter/nvim-treesitter-textobjects" },
   { src = "https://github.com/neovim-treesitter/nvim-treesitter" },
   { src = "https://github.com/folke/snacks.nvim" },
@@ -22,13 +22,13 @@ local specs = {
   { src = "https://github.com/rebelot/heirline.nvim" },
   { src = "https://github.com/nvim-mini/mini.diff" },
   { src = "https://github.com/nvim-mini/mini.icons" },
-  { src = "https://github.com/kevinhwang91/nvim-ufo.git" },
+  { src = "https://github.com/kevinhwang91/nvim-ufo" },
   { src = "https://github.com/hedyhli/outline.nvim" },
   { src = "https://github.com/folke/todo-comments.nvim" },
   { src = "https://github.com/MeanderingProgrammer/render-markdown.nvim" },
   -- vcs
-  { src = "https://github.com/nicolasgb/jj.nvim" },
   { src = "https://github.com/julienvincent/hunk.nvim" },
+  { src = "https://github.com/nicolasgb/jj.nvim" },
   -- dependencies
   { src = "https://github.com/neovim/nvim-lspconfig" },
   { src = "https://github.com/MunifTanjim/nui.nvim" },
@@ -36,9 +36,9 @@ local specs = {
   { src = "https://github.com/neovim-treesitter/treesitter-parser-registry" },
 }
 
-vim.pack.add(specs)
+local disabled_specs = {}
 
-local function get_plugin_names(arg_lead)
+local function get_names_from_vimpack(arg_lead)
   local installed = vim.pack.get(nil, { info = false })
   local names = {}
   for _, p in ipairs(installed) do
@@ -51,52 +51,114 @@ local function get_plugin_names(arg_lead)
   return names
 end
 
+local function get_name_from_spec(spec)
+  local name
+  if spec.name then
+    name = spec.name
+  else
+    local src = spec.src
+    name = src:match("([^/]+)$"):gsub("%.git$", "")
+  end
+  return name
+end
+
 local function load_plugins()
-  local files = vim.api.nvim_get_runtime_file("lua/plugins/**/*.lua", true)
-  for _, file in ipairs(files) do
-    local normalized_file = file:gsub("\\", "/")
-    local module_path = normalized_file:match("lua/(plugins/.*)%.lua$")
-    if module_path then
-      local module_name = module_path:gsub("/", ".")
-      local ok, err = pcall(require, module_name)
-      if not ok then
-        vim.notify("Failed load module: " .. module_name .. "\n" .. tostring(err), vim.log.levels.ERROR)
+  for _, spec in ipairs(active_specs) do
+    local name = get_name_from_spec(spec):gsub("%.", "-")
+    local files = vim.api.nvim_get_runtime_file("lua/plugins/**/" .. name .. ".lua", false)
+    if files[1] then
+      local normalized = files[1]:gsub("\\", "/")
+      local module_path = normalized:match("lua/(plugins/.+)%.lua$")
+      if module_path then
+        local ok, err = pcall(require, module_path:gsub("/", "."))
+        if not ok then
+          vim.notify("Failed to load: " .. module_path .. "\n" .. tostring(err), vim.log.levels.ERROR)
+        end
       end
     end
   end
 end
 
-vim.api.nvim_create_user_command("PackUpdate", function(opts)
-  local targets = #opts.fargs > 0 and opts.fargs or nil
-  local force = opts.bang
-  if targets then
-    vim.notify("Checking updates for: " .. table.concat(targets, ", "), vim.log.levels.INFO)
-  else
-    vim.notify("Checking updates for all plugins...", vim.log.levels.INFO)
+---Auto delete the orhpan plugins.
+---@param active table
+---@param disabled table
+local function sync(active, disabled)
+  active = active or {}
+  disabled = disabled or {}
+
+  local protected_names = {}
+
+  -- protect the plugin in specs
+  for _, spec in ipairs(active) do
+    protected_names[get_name_from_spec(spec)] = true
   end
-  vim.pack.update(targets, { force = force })
-end, {
-  nargs = "*",
-  bang = true, -- support ! for force update
-  complete = get_plugin_names,
-  desc = "Update plugins (use ! to skip confirmation)",
-})
+  for _, spec in ipairs(disabled) do
+    local name = get_name_from_spec(spec)
+    protected_names[name] = true
+  end
+
+  -- scan the plugins in the disk
+  local installed_plugins = {}
+  local pack_dir = vim.fn.stdpath("data") .. "/site/pack"
+  local packages = vim.fn.expand(pack_dir .. "/*", false, true)
+  for _, pkg in ipairs(packages) do
+    for _, type_dir in ipairs({ "start", "opt" }) do
+      local path = pkg .. "/" .. type_dir
+      if vim.fn.isdirectory(path) == 1 then
+        local dirs = vim.fn.expand(path .. "/*", false, true)
+        for _, dir in ipairs(dirs) do
+          local name = dir:match("([^/]+)$")
+          if name ~= "README.md" and name ~= "doc" then
+            table.insert(installed_plugins, name)
+          end
+        end
+      end
+    end
+  end
+
+  -- delete the orphaned plugins
+  local to_delete = {}
+  for _, installed in ipairs(installed_plugins) do
+    if not protected_names[installed] then
+      table.insert(to_delete, installed)
+    end
+  end
+  if #to_delete > 0 then
+    vim.schedule(function()
+      vim.notify(" Clean Up Orphaned Plugins: " .. table.concat(to_delete, ", "), vim.log.levels.INFO)
+      vim.pack.del(to_delete)
+    end)
+  end
+end
+
+vim.api.nvim_create_user_command(
+  "PackUpdate",
+  function(opts)
+    local targets = #opts.fargs > 0 and opts.fargs or nil
+    local force = opts.bang
+    if targets then
+      vim.notify("Checking updates for: " .. table.concat(targets, ", "), vim.log.levels.INFO)
+    else
+      vim.notify("Checking updates for all plugins...", vim.log.levels.INFO)
+    end
+    vim.pack.update(targets, { force = force })
+  end,
+  { nargs = "*", bang = true, complete = get_names_from_vimpack, desc = "Update plugins (use ! to skip confirmation)" }
+)
 
 vim.api.nvim_create_user_command("PackDel", function(opts)
   vim.pack.del(opts.fargs)
-end, { nargs = "+", complete = get_plugin_names, desc = "Delete plugins" })
+end, { nargs = "+", complete = get_names_from_vimpack, desc = "Delete plugins" })
 
 vim.api.nvim_create_user_command("PackStatus", function(opts)
   local targets = #opts.fargs > 0 and opts.fargs or nil
   vim.pack.update(targets, { offline = true })
-end, {
-  nargs = "*",
-  complete = get_plugin_names,
-  desc = "Check plugins status without downloading",
-})
+end, { nargs = "*", complete = get_names_from_vimpack, desc = "Check plugins status without downloading" })
 
-vim.keymap.set("n", "<leader>P", "<cmd>PackStatus<cr>", { desc = "Pack status" })
+vim.pack.add(active_specs)
+
+sync(active_specs, disabled_specs)
 
 load_plugins()
 
--- TODO: need sync
+vim.keymap.set("n", "<leader>P", "<cmd>PackStatus<cr>", { desc = "Pack status" })
